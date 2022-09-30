1324c69b453b461f69a22361122fe6a879e42d99
Dayyan Muhammad <m.dayyan367@gmail.com>
2022-09-30 16:02:47 -0600

    finish

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 #  coding: utf-8
 import socketserver
-from urllib import response
 import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
@@ -45,7 +44,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
                 'HTTP/1.1 404 Not Found\r\n'.encode('utf-8'))
             return
 
-        if(self.data.decode('utf-8').split(" ")[0] != "GET"):
+        if (self.data.decode('utf-8').split(" ")[0] != "GET"):
             self.request.sendall(
                 'HTTP/1.1 405 Not Found\r\n'.encode('utf-8'))
             return
@@ -53,40 +52,18 @@ class MyWebServer(socketserver.BaseRequestHandler):
         if (".ico" in reqPage):
             return
 
-        # print(reqPage)
-
         myFile = "./www/{}".format("/".join(reqPageSplit))
 
-        if (myFile[-1] == "/" and "." not in myFile[1:]):
-            print("CHANGED")
+        if (reqPage[-1] != "/" and "." not in myFile[1:] and reqPage != "/"):
             self.request.sendall(
-                ('HTTP/1.1 301 Moved Permanently\r\nLocation: {}/index.html'.format(reqPageSplit[0])).encode('utf-8'))
+                ('HTTP/1.1 301 Moved Permanently\r\nLocation: {}/'.format(reqPageSplit[0])).encode('utf-8'))
             return
-        # print(myFile)
+
         if (myFile[-1] == "/"):
             myFile += "index.html"
+
         if ("." not in myFile[1:]):
-            # print("change")
             myFile += "/index.html"
-
-        # print(myFile)
-        # print("./www/{}".format("/".join(reqPageSplit)))
-        # if (len(reqPageSplit) == 0):
-        #     print("./www/index.html")
-        # else:
-        #     print("./www/{}".format("".join(reqPageSplit)))
-
-        # if (len(reqPageSplit) == 0 or 'index.html' in reqPageSplit[0]):
-        #     myFile = "./www/index.html"
-        # else:
-        #     if (".css" in reqPageSplit[0]):
-        #         # print(reqPage)
-        #         myFile = "./www/base.css"
-        #     elif ("deep" in reqPageSplit[0]):
-        #         if (len(reqPageSplit) > 1 and ".css" in reqPageSplit[1]):
-        #             myFile = "./www/deep/deep.css"
-        #         else:
-        #             myFile = "./www/deep/index.html"
 
         try:
             file = open(myFile, 'rb')
@@ -106,12 +83,7 @@ class MyWebServer(socketserver.BaseRequestHandler):
             header = 'HTTP/1.1 404 Not Found\n\n'
             response = ''.encode('utf-8')
 
-        # print("header", header, type(header))
         final_response = header.encode('utf-8')
-        # print("final", final_response, type(final_response))
-        # if (type(response) == "str"):
-        # print("Not bytes", response)
-        # print("response", response, type(response))
         final_response += response
 
         self.request.sendall(final_response)
@@ -121,9 +93,6 @@ if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
     socketserver.TCPServer.allow_reuse_address = True
-    # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
     server.serve_forever()
